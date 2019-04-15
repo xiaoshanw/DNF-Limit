66d40cc3776dceeda81e8afd572291e0648e0d07
ddknight <cc199026@gmail.com>
2019-04-15 10:39:13 +0800

    update solution config to VS Studio 2019

--- D♂N♂F神秘力量/My Project/Application.Designer.vb
+++ D♂N♂F神秘力量/My Project/Application.Designer.vb
@@ -14,7 +14,7 @@ Option Explicit On
 
 Namespace My
     
-    '注意: 此文件是自动生成的；请不要直接修改它。若要进行更改，
+    '注意:此文件是自动生成的；请勿直接进行修改。若要更改，
     ' 或者如果您在此文件中遇到生成错误，请转至项目设计器
     ' (转至“项目属性”或在解决方案资源管理器中双击“我的项目”节点)，
     ' 然后在“应用程序”选项卡中进行更改。
@@ -25,7 +25,7 @@ Namespace My
         Public Sub New()
             MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
             Me.IsSingleInstance = true
-            Me.EnableVisualStyles = true
+            Me.EnableVisualStyles = false
             Me.SaveMySettingsOnExit = true
             Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
         End Sub
